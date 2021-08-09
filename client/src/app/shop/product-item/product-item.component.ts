4a360d2ad60b174a54516e5cc0b57112f4cb8d70
smc-dotexe <shanelgmcguire@gmail.com>
2021-08-08 23:42:06 -0600

    worked on front-end functions. pagination, filtering and searching

---
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss']
+})
+export class ProductItemComponent implements OnInit {
+  @Input() product: IProduct = {
+    id: 0,
+    name: '',
+    description: '',
+    price: 0,
+    pictureUrl: '',
+    productType: '',
+    productBrand: ''
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
